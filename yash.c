622701afe0e6193d87410716fe66a09884a9c00a
jlove96 <bluec77@sbcglobal.net>
2017-09-15 16:23:40 -0500

    First Commit

---
+++ yash.c
@@ -0,0 +1,17 @@
+//Johnathon Love (jal5622)
+//EE461S - Yerraballi
+//Project 1
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]){
+	char inputString[2000 + 1];
+	
+
+
+}
